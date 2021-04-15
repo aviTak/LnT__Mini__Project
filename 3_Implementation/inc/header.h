d4b3fb69f4a4cd9d21bb87ba3880ed4344d5ab4e
Avi Takiyar <avi.takiyar@gmail.com>
2021-04-15 15:23:15 +0530

    changes

--- 3_Implementation/inc/header.h
+++ 3_Implementation/inc/header.h
@@ -40,30 +40,7 @@ typedef struct Book{
 	struct Book*next;
 }book;
 
-book*GreatLink()
-{
-	book*h, *tail, *p;
-	h=tail=(book*)malloc(sizeof(book));
-	h->next = NULL;
-	int n = 0;
-	printf("Enter the number of books to be entered:");
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		p = (book*)malloc(sizeof(book));
-		printf("Book Title:");
-		scanf("%s",&p->date.name);
-		printf("IBSN:");
-		scanf("%s",&p->date.ISBN);
-		printf("price:");
-		scanf("%s",&p->date.price);
-		p->next = NULL;
-		tail->next = p; 
-		tail = p;
-	}
 
-	return h;
-}
 /**
  * @brief Display the records
  * @param book
@@ -95,6 +72,12 @@ void choose(book*h);
 /**
  * @brief Display menu
 */
+
+/**
+ * @brief Great Link
+*/
+
+book*GreatLink();
 
 void menu();
 
